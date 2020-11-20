e11b3b04404db2ce1cca3a1f6c3d744b3bb75b9d
felipe <felipedemaria@hotmail.com>
2020-11-20 00:34:17 -0300

    indentation

--- src/main/java/com/example/spring/controllers/TvShowController.java
+++ src/main/java/com/example/spring/controllers/TvShowController.java
@@ -6,7 +6,6 @@ import com.example.spring.services.BasicTvShowInfoService;
 import com.example.spring.services.TvShowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
