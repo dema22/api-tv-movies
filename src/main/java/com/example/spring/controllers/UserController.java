8a4bfe5476bf79f0b4e83e0671674235f51106ef
felipe <felipedemaria@hotmail.com>
2020-12-06 19:55:10 -0300

    refactor security

--- src/main/java/com/example/spring/controllers/UserController.java
+++ src/main/java/com/example/spring/controllers/UserController.java
@@ -70,7 +70,7 @@ public class UserController {
     public void addUser(@RequestBody User user) {
         userService.addUser(user);
     }
-    
+
     @GetMapping("/")
     public List<User> getAllUser(){
         List<User> users = userService.getAllUsers();
