9d57e0c4d2c2e16bbee1989ad58fafec980a43eb
felipe <felipedemaria@hotmail.com>
2020-12-25 01:23:47 -0300

    refactored swagger config

--- src/main/java/com/example/spring/config/SpringFoxConfiguration.java
+++ src/main/java/com/example/spring/config/SpringFoxConfiguration.java
@@ -2,15 +2,18 @@ package com.example.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.*;
+
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfiguration {
+/*public class SpringFoxConfiguration {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -18,5 +21,29 @@ public class SpringFoxConfiguration {
                 .apis(RequestHandlerSelectors.basePackage("com.example.spring"))
                 .paths(PathSelectors.any())
                 .build();
+    }
+}*/
+
+public class SpringFoxConfiguration {
+    private static final Set<String> DEFAULT_PRODUCES_CONSUMES = new HashSet<String>(Arrays.asList("application/json"));
+
+    @Bean
+    public Docket api() {
+        ParameterBuilder parameterBuilder = new ParameterBuilder();
+        parameterBuilder.name("Authorization")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .description("JWT token")
+                .required(false)
+                .build();
+        List<Parameter> parameters = new ArrayList<>();
+        parameters.add(parameterBuilder.build());
+        return new Docket(DocumentationType.SWAGGER_2)
+                .produces(DEFAULT_PRODUCES_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_CONSUMES)
+                .select()
+                .build()
+                // Setting globalOperationParameters ensures that authentication header is applied to all APIs
+                .globalOperationParameters(parameters);
     }
 }
